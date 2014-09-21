354eaa59b9213f16a3186805db4827781cfd663f
Giorgia <giorgia1london@gmail.com>
2014-09-21 12:53:45 +0100

    student's count

--- directory.rb
+++ directory.rb
@@ -1,5 +1,9 @@
+student_count = 3
 puts "The name of students in my lab"
 puts "------------------------------"
 puts "Pablo"
 puts "Yorik"
 puts "Ciaky"
+print "Overall we have "
+print student_count
+puts " students"
