4ecb55f92ef1770ba592649282fe31bcfb3784e7
Giorgia <giorgia1london@gmail.com>
2014-09-22 16:16:54 +0100

    directory.rb

--- directory.rb
+++ directory.rb
@@ -29,7 +29,7 @@ def input_students
 	while !name.empty? do
 		#add the student hash to the array
 		students << {:name => name, :cohort => cohort}
-		puts "Now we have #{students.length} students"
+		puts "Now we have #{students.length} student"
 		#get another name from the user
 		name = gets.chomp
 		puts "Which cohort"
@@ -39,7 +39,32 @@ def input_students
 	students
 end
 
-students = input_students
-print_header
-print(students)
-print_footer(students)
+
+def interactive_menu
+	students = []
+	loop do
+	# 1. print the menu and ask the user what to do
+	puts "1. Input the students"
+	puts "2. Show the students"
+	puts "9. Exit"
+	# 2. read the input and save it into a variable
+	selection = gets.chomp
+	# 3. do what the user has asked
+	case selection
+	when "1"
+		students = input_students
+	when "2"
+		print_header
+		print(students)
+		print_footer(students)
+	when "9"
+		exit #this will cause the programm to terminate
+	else
+		puts "I don't know what you meant, try again"
+	end
+	end
+end 
+
+interactive_menu
+
+
