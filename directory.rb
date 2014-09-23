ccc6c21e8e6c603a2324332bd7df5680e95cae3f
Giorgia <giorgia1london@gmail.com>
2014-09-23 13:20:40 +0100

    End of first part_student directory

--- directory.rb
+++ directory.rb
@@ -1,70 +1,96 @@
+@students = []
+
 def print_header
-	puts "Students now "
-	puts "-------------------"
+  puts "The students of my cohort at Makers Academy"
+  puts "-------------"
 end
 
-
-def print(students)
-	students.each_with_index do |student, index|
-	puts "#{index + 1}: #{student[:name]} (#{student[:cohort]} cohort)"
-
-	end
+def print_students_list
+  @students.each do |student|
+    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  end
 end
 
-
-def print_footer(names)
-	puts "Overall, we have #{names.length} students"
+def print_footer    
+  puts "Overall, we have #{@students.length} great students"
 end
 
 def input_students
-	puts "Please enter the names of students"
-	puts "To finish, just hit return twice"
-	#create the empty array that will store th students' names
-	students = []
-	#get the first name
-	name = gets.chomp
-	puts "Whcih cohort?"
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  # get the first name
+  name = gets.chomp
+  puts "Which cohort?"
+  cohort = gets.chomp
+  # while the name is not empty, repeat this code
+  while !name.empty? do    
+    # add the student hash to the array
+    @students << {:name => name, :cohort => cohort}    
+    puts "Now we have #{@students.length} students"
+    # get another name from the user
+    name = gets.chomp
+    puts "Which cohort"
 	cohort = gets.chomp
-	#while the name is not empty, repeat this code
-	while !name.empty? do
-		#add the student hash to the array
-		students << {:name => name, :cohort => cohort}
-		puts "Now we have #{students.length} student"
-		#get another name from the user
-		name = gets.chomp
-		puts "Which cohort"
-		cohort = gets.chomp
-	end
-	#return the array of students
-	students
+  end
+end
+
+def print_menu    
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "3. Save the list to students.csv"
+  puts "9. Exit" # 9 because we'll be adding more items  
+end
+
+def show_students
+  print_header
+  print_students_list
+  print_footer
 end
 
 
-def interactive_menu
-	students = []
-	loop do
-	# 1. print the menu and ask the user what to do
-	puts "1. Input the students"
-	puts "2. Show the students"
-	puts "9. Exit"
-	# 2. read the input and save it into a variable
-	selection = gets.chomp
-	# 3. do what the user has asked
-	case selection
-	when "1"
-		students = input_students
-	when "2"
-		print_header
-		print(students)
-		print_footer(students)
-	when "9"
-		exit #this will cause the programm to terminate
-	else
-		puts "I don't know what you meant, try again"
+
+
+
+def save_students
+	#open the file for writing 
+	file = File.open("students.csv", "w")
+	#iterate over the array of students
+	@students.each do |student|
+		student_data = [student[:name], student[:cohort]]
+		csv_line = student_data.join (",")
+		file.puts csv_line
 	end
-	end
-end 
+	file.close
+end
+
+
+def process(selection)
+  case selection
+  when "1"
+    input_students
+  when "2"
+    show_students
+  when "3"
+    save_students
+  when "9"
+    exit
+  else
+    puts "I don't know what you mean, try again"
+  end
+end
+
+
+def interactive_menu  
+  loop do
+    print_menu        
+    process(gets.chomp)
+  end
+end
 
 interactive_menu
 
+
+
+
+	
 
